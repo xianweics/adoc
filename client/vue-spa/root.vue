2bc0fbc579712d1c9fe1632fb4eb2326ee9ca3e2
xianweiwu <xian-wei.wu@microfocus.com>
2020-05-29 13:48:05 +0800

    update: add img demo.

--- client/vue-spa/root.vue
+++ client/vue-spa/root.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="content">
         <h1 @click="test">Hello from Vue 2 spa</h1>
+        <div><img :src="imgSrc" width="200px"></div>
         <a href="/react-spa">from vue to react</a>
     </div>
 </template>
 
 <script>
+  import src from './office365.png';
   export default {
-    methods:{
-      async test(){
+    data () {
+      return {
+        imgSrc: src
+      }
+    },
+    methods: {
+      async test () {
         let result = await this.$http.get('users');
         console.info(result)
       }

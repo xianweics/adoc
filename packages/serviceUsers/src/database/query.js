771a571f58519184de553498514c1b066e561922
Xianweiwu <xianweics@163.com>
2022-03-26 01:54:54 +0800

    set port for mysql

---
+++ packages/serviceUsers/src/database/query.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql';
+import config from './config.js';
+
+const { database } = config;
+const pool = mysql.createPool({
+  host: database.host,
+  user: database.username,
+  port: database.port,
+  password: database.password,
+  database: database.database
+});
+console.info(pool, database.host);
+export default function query (sql, values) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) reject(err);
+      
+      connection.query(sql, values, (err, rows) => {
+        if (err) reject(err);
+        
+        resolve(rows);
+        connection.release();
+      });
+    });
+  });
+}
